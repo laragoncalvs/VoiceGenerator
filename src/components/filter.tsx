10741f45ccd5fa2a0e19bbfcfd1d72ebddd440ce
Lara Gonçalves <gs_lara@yahoo.com>
2024-08-11 11:14:21 -0300

    key

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -40,15 +40,15 @@ export function Filter({ voiceOptions }: { voiceOptions: VoiceOptions }) {
       {voiceOptions &&
         Object.entries(voiceOptions).map(([key, value]) => (
           value && (
-            <div className="p-1">
+            <div className="p-1" key={key} >
 
-              <Select key={key} >
+              <Select >
                 <SelectTrigger className="w-[180px]">
                   <SelectValue placeholder={key} />
                 </SelectTrigger>
                 <SelectContent>
                   {value.map((item: any, index: any) => (
-                    <label className="relative flex items-center p-1 rounded-full cursor-pointer"  key={item}>
+                    <label className="relative flex items-center p-1 rounded-full cursor-pointer" key={item}>
                       <input
                         className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-indigo-500 checked:bg-indigo-500 checked:before:bg-indigo-500 hover:before:opacity-10" type="checkbox"
                         id="indigo"
@@ -58,7 +58,7 @@ export function Filter({ voiceOptions }: { voiceOptions: VoiceOptions }) {
                       />
                       <span className="ml-2">
 
-                      {item}
+                        {item}
                       </span>
                     </label>
                   ))}
